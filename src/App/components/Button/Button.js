57ff8cb53f49c0669b2b0489e60082ae8a45f986
RodolpheGaste <rodolphe.gaste@ima.eu>
2022-04-13 13:31:27 +0100

    CSS Button + affichage

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Button.css'
 
 const Button = (props)=>{
     console.log(props);
-    return <button>Demat</button>;
+    return <button className="Button">{props.text}</button>;
 
 }
 
